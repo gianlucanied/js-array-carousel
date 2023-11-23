6094fb5a376e11c5ad49c7cf1984676436c7e0a4
gianlucanied <gianluca.nieddu96@gmail.com>
2023-11-23 16:03:00 +0100

    add: style in vari div add: JS

---
+++ script.js
@@ -0,0 +1,23 @@
+// Creare un carosello di img
+
+const items = document.getElementsByClassName('item');
+console.log(items);
+
+let activeItem = 0;
+
+const next = document.querySelector('.next');
+
+next.addEventListener('click', function () {
+
+    if (activeItem < items.length -1) {
+        items[activeItem].classList.remove('.active');
+
+        activeItem++;
+
+        items[activeItem].classList.add('.active');
+
+        if (activeItem === items.length -1) {
+            next.classList.add('hidden');
+        }
+    }
+})
